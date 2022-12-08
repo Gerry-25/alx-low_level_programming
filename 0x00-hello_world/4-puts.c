b2e03116b6ae58d942dcb719f4c738c03c1ef955
Gerry-25 <gerrygoubalan21@gmail.com>
2022-12-08 05:31:02 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  *
- * main - Entry point
+ *  * main - Entry point
  *
- * 
+ *  *
  *
- * Return: Always 0 (Success)
+ *  * Return: Always 0 (Success)
  *
  */
 
@@ -15,7 +15,7 @@ int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 
 }
+
